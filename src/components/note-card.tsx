f5f02416d2af9b1c44eeccc9b12f783dd137eeca
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-02-06 14:24:28 -0300

    add modal new-note-card

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
 interface NoteCardProps {
   date: Date
   content: string
@@ -18,7 +19,10 @@ export function NoteCard({ date, content }: NoteCardProps) {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 bg-black/50" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 flex h-[60vh] w-full max-w-[640px] -translate-x-1/2 -translate-y-1/2 flex-col rounded-md bg-slate-700 outline-none">
+        <Dialog.Content className="fixed left-1/2 top-1/2 flex h-[60vh] w-full max-w-[640px] -translate-x-1/2 -translate-y-1/2 flex-col overflow-hidden rounded-md bg-slate-700 outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="h-5 w-5" />
+          </Dialog.Close>
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-300">
               {formatDistanceToNow(date.toISOString(), {
@@ -28,6 +32,15 @@ export function NoteCard({ date, content }: NoteCardProps) {
             </span>
             <p className="text-sm leading-6 text-slate-400">{content}</p>
           </div>
+          <button
+            type="button"
+            className="group w-full bg-slate-800 py-4 text-center text-sm font-medium text-slate-300 outline-none"
+          >
+            Deseja{' '}
+            <span className="text-red-400 group-hover:underline ">
+              apagar essa nota?
+            </span>
+          </button>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
